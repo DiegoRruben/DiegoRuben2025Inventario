8b6a4617ed7780b80bf3983818138f244363cd7b
Diego Rubén <dieguiplayex@gmail.com>
2025-03-20 20:17:53 -0600

    Inventario3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola estoy haciendo un inventario");
+
+      Scanner scanner = new Scanner(System.in);
+      ArrayList<String>productos = new ArrayList<>();
+      ArrayList<Integer>cantidades = new ArrayList<>();
+
 
 
 
